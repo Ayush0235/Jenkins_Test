7da30c6fa9bb379427205425e3d134ac98b6904a
Ayush0235 <121756791+Ayush0235@users.noreply.github.com>
2023-02-21 14:41:13 +0530

    Update Jenkins.java

--- src/Jenkins.java
+++ src/Jenkins.java
@@ -3,7 +3,7 @@ public class Jenkins {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello this is Ayush Mehta here");
+		System.out.println("Hello this is Ayush Mehta");
 		
 	}
 
